f649293f43ea58dca3b776a549c6a6d84c7e119b
Gamingwithchuqmuq <95252012+CuriousCoder1232@users.noreply.github.com>
2022-09-26 10:31:25 -0400

    Create a start game script

---
+++ startgame.java
@@ -0,0 +1,12 @@
+function startGame() {
+  let squares = document.querySelectorAll(".grid div");
+  randomApple(squares);
+  //random apple
+  direction = 1;
+  scoreDisplay.innerHTML = score;
+  intervalTime = 1000;
+  currentSnake = [2, 1, 0];
+  currentIndex = 0;
+  currentSnake.forEach((index) => squares[index].classList.add("snake"));
+  interval = setInterval(moveOutcome, intervalTime);
+}
