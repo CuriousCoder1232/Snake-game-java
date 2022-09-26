778118017e552445dce09cbfe4d32437f3b9c264
Gamingwithchuqmuq <95252012+CuriousCoder1232@users.noreply.github.com>
2022-09-26 10:32:28 -0400

    Create movesnake.java

---
+++ movesnake.java
@@ -0,0 +1,8 @@
+function moveSnake(squares) {
+  let tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+  // movement ends here
+  eatApple(squares, tail);
+  squares[currentSnake[0]].classList.add("snake");
+}
