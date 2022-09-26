90630da1dcedf884a6e6f903833c5d97611a7366
Gamingwithchuqmuq <95252012+CuriousCoder1232@users.noreply.github.com>
2022-09-26 10:33:27 -0400

    EAT THE DAMN APPLE SNAKE

---
+++ eatingthedamnapple.java
@@ -0,0 +1,13 @@
+function eatApple(squares, tail) {
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    squares[currentSnake[0]].classList.remove("apple");
+    squares[tail].classList.add("snake");
+    currentSnake.push(tail);
+    randomApple(squares);
+    score++;
+    scoreDisplay.textContent = score;
+    clearInterval(interval);
+    intervalTime = intervalTime * speed;
+    interval = setInterval(moveOutcome, intervalTime);
+  }
+}
