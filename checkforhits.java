27d8baa9125ce5692c7ee16cd3b6bf8c42473c3e
Gamingwithchuqmuq <95252012+CuriousCoder1232@users.noreply.github.com>
2022-09-26 10:32:52 -0400

    Create checkforhits.java

---
+++ checkforhits.java
@@ -0,0 +1,13 @@
+function checkForHits(squares) {
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width <= 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
